fbaae7e5733d13b801b079dae1314a6e9dbc3d41
Solomon Bekele <solubman28@gmail.com>
2025-10-06 20:04:58 +0400

    add client side

--- server/server.js
+++ server/server.js
@@ -12,6 +12,61 @@ const port = process.env.PORT || 3004;
 app.use(epress.json());
 app.use(cors());
 app.use('/ingest_data', data);
+app.get('/results', (req, res) => {
+  const passedType = req.query.type;
+  const passedMag = req.query.mag;
+  const passedLocation = req.query.location;
+  const passedDateRange = req.query.dateRange;
+  const passedSortOption = req.query.sortOption;
+  
+
+  async function sendESRequest() {
+    const body = await client.search({
+      index: 'earthquakes',
+      body: {
+        sort: [
+          {
+            mag: {
+              order: passedSortOption,
+            },
+          },
+        ],
+        size: 300,
+        query: {
+          bool: {
+            filter: [
+              {
+                term: { type: passedType },
+              },
+              {
+                range: {
+                  mag: {
+                    gte: passedMag,
+                  },
+                },
+              },
+              {
+                match: { place: passedLocation },
+              },
+              // for those who use prettier, make sure there is no whitespace.
+              {
+                range: {
+                  '@timestamp': {
+                    gte: `now-${passedDateRange}d/d`,
+                    lt: 'now/d',
+                  },
+                },
+              },
+            ],
+          },
+        },
+      },
+    });
+    res.json(body.hits.hits);
+  }
+  sendESRequest();
+});
+
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
